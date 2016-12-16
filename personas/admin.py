472581d9cb66453f65c0a1aee40a2fa332ef74af
Urko <zaszur@gmail.com>
2016-12-16 11:33:44 +0100

    Primer commit tras la creaciond el proyecto y el modelo de la aplicacion Persona

---
+++ personas/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from personas.models import Persona
+
+admin.site.register(Persona)
---
+++ personas/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+HOMBRE = "H"
+MUJER = "M"
+
+GENERO = (
+    (HOMBRE, "Hombre"),
+    (MUJER, "Mujer"),
+)
+
+SI = "S"
+NO = "N"
+
+PADRON = (
+    (SI, 'Si'),
+    (NO, 'No'),
+)
+
+
+class Persona(models.Model):
+
+    nombre = models.CharField(max_length=100)  # Nombre de la persona (obligatorio)
+    primer_apellido = models.CharField(max_length=100)  # Primer apellido de la persona (obligatorio)
+    segundo_apellido = models.CharField(max_length=100)  # Segundo apellido de la persona
+    sexo = models.CharField(max_length=1, choices=GENERO, default=HOMBRE)  # Sexo de la persona
+    fecha_nacimiento = models.DateField()  # Fecha de nacimiento de la persona
+    direccion = models.CharField(max_length=100)  # Dirección de la persona
+    telefono = models.CharField(max_length=9)  # Teléfono de la persona
+    empadronamiento = models.CharField(max_length=1, choices=PADRON, default=NO)
+    fecha_alta = models.DateTimeField(auto_now_add=True)  # Guarda la fecha de alta de la persona (automática)
+    fecha_modificacion = models.DateTimeField(auto_now=True)  # Guarda la fecha de la última modificación de la ficha
+
+    def __str__(self):
+        return self.nombre
